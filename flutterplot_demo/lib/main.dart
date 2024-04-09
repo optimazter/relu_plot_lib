605c51d9da020e9075ec46e6fae99ebdeb32dfc7
optimazter <adrianhjertholmspill@gmail.com>
2024-04-09 20:21:06 +0200

    Demo simplified

--- flutterplot_demo/lib/main.dart
+++ flutterplot_demo/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutterplot/flutterplot.dart';
 
@@ -36,26 +35,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
 
-  double f1(num x) {
-    return 2 * pow(x, 2) / 1000 + 2*x + 40;
-  }
-
-  double f2(num x) {
-    return 2 * pow(x, 2)/ 1000 + 3 * x + 500;
-  }
 
 
   @override
   Widget build(BuildContext context) {
 
-    List<double> X = [];
-    List<double> y1 = [];
-    List<double> y2 = [];
-    for (int i = -500000; i < 500000; i++) {
-      X.add(i.toDouble());
-      y1.add(f1(i));
-      y2.add(f2(i));
-    }
+    final x = List<double>.generate(10000, (i) => i + 1);
+    final y = x.map((x) => 2 * x + 3).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -64,44 +50,42 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Plot(
-          padding: 30,
-          xUnit: 'Hz',
-          yUnit: 'dB',
-          graphs: [
-            Graph(
-              X: X, 
-              Y: y1,
-              color: Colors.blue,
-              crosshair: Crosshair(label: 'Test1', active: true, yPadding: 10),
-              annotations: [Annotation(child: const SizedBox(
-                width: 100,
-                height: 80,
-                child: Card.filled(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Plot(
+            padding: 30,
+            xUnit: 'Hz',
+            yUnit: 'dB',
+            xLog: true,
+            yLog: false,
+            ticksFractionDigits: 0,
+            numYTicks: 5,
+            graphs: [
+              Graph(
+                x: x, 
+                y: y,
                 color: Colors.blue,
-                child: Text('Test'),
-              )
-              )
-            )]
-            ),
-            // Graph(
-            //   X: X, 
-            //   Y: y2,
-            //   color: Colors.amber,
-            //   crosshair: Crosshair(label: 'Test2', active: true, yPadding: 20),
-            //    annotations: [Annotation(child: const SizedBox(
-            //     width: 100,
-            //     height: 80,
-            //     child: Card.filled(
-            //     color: Colors.amber,
-            //     child: Text('hei 2'),
-            //   )
-            //   )
-            // )]
-            //   )
-            ],
+                crosshairs: [
+                  Crosshair(
+                    label: 'Crosshair', 
+                    active: true, 
+                    yPadding: 10, 
+                    color: Colors.blue)],
+                annotations: [
+                  Annotation(child: const SizedBox(
+                    width: 100,
+                    height: 80,
+                    child: Card.filled(
+                    color: Colors.blue,
+                    child: Text('Simple Annotation'),
+                )
+                )
+              )]
+              ),
+              ],
         )
-    )
+       )
+      ),
     );
   }
 }
