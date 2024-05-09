d7e36a242f058cb45d7a9013cd612715142ac41e
optimazter <adrianhjertholmspill@gmail.com>
2024-05-09 15:24:32 +0200

    Updated fraction digits

--- flutterplot/test/widgets_test/plot_view_test.dart
+++ flutterplot/test/widgets_test/plot_view_test.dart
@@ -1,28 +1,28 @@
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterplot/flutterplot.dart';
-import 'package:flutterplot/src/widgets/plot_view.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutterplot/flutterplot.dart';
+// import 'package:flutterplot/src/widgets/plot_view.dart';
 
 
 
 
 
-void main() {
+// void main() {
 
-  // testWidgets(' ', (WidgetTester tester) async {
-  //   await tester.pumpWidget(
-  //     PlotView(plot: 
-  //       Plot(
-  //         xTicks: const [1, 2],
-  //         yTicks: const [1, 2],
-  //         ticksFractionDigits: 0,
-  //         graphs: [
-  //           Graph(
-  //             x: [1, 2, 3, 4, 5], 
-  //             y: [1, 2, 3, 4, 5]
-  //         )],
-  //       )
-  //     )
-  //   );
-  // });
-}
+//   testWidgets(' ', (WidgetTester tester) async {
+//     await tester.pumpWidget(
+//       PlotView(plot: 
+//         Plot(
+//           xTicks: const [1, 2],
+//           yTicks: const [1, 2],
+//           ticksFractionDigits: 0,
+//           graphs: [
+//             Graph(
+//               x: [1, 2, 3, 4, 5], 
+//               y: [1, 2, 3, 4, 5]
+//           )],
+//         )
+//       )
+//     );
+//   });
+// }
