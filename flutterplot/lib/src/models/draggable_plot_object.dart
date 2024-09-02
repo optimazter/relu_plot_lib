4808b8cba7be802da99791d125345372a8979fd3
nordicAdrian <adrian@nordicneurolab.com>
2024-09-02 17:06:55 +0200

    onDragStart and onDragEnd now refers to the PlotObject instead of its position

--- flutterplot/lib/src/models/draggable_plot_object.dart
+++ flutterplot/lib/src/models/draggable_plot_object.dart
@@ -8,8 +8,8 @@ abstract class DraggablePlotObject {
     required this.width,
     required this.height,
     this.position = Offset.infinite,
-    final Function(Offset position)? onDragStart,
-    final Function(Offset position)? onDragEnd
+    final Function(DraggablePlotObject position)? onDragStart,
+    final Function(DraggablePlotObject position)? onDragEnd
   }) : 
   _onDragStart = onDragStart,
   _onDragEnd = onDragEnd;
@@ -21,18 +21,18 @@ abstract class DraggablePlotObject {
 
 
   /// Function called every time the PlotObject is hit.
-  final Function(Offset position)? _onDragStart;
+  final Function(DraggablePlotObject position)? _onDragStart;
 
   /// Function called every time the PlotObject has been moved
-  final Function(Offset position)? _onDragEnd;
+  final Function(DraggablePlotObject position)? _onDragEnd;
 
 
   void onDragStart() {
-    _onDragStart?.call(position);
+    _onDragStart?.call(this);
   }
 
   void onDragEnd() {
-    _onDragEnd?.call(position);
+    _onDragEnd?.call(this);
   }
 
   void onDrag(PointerMoveEvent event);
